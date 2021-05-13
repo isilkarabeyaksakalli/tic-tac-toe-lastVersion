25f3c6522b8de7f5f1cbbb445f123169c3466e54
isilkarabeyaksakalli <54484547+isilkarabeyaksakalli@users.noreply.github.com>
2021-05-13 16:26:13 +0300

    first commit

---
+++ views/PlayerViews.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const exports = {};
+
+// Player views must be extended.
+// It does not have its own Wrapper view.
+
+exports.WaitingForResults = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Waiting for results...
+      </div>
+    );
+  }
+}
+
+exports.Done = class extends React.Component {
+  render() {
+    const {outcome} = this.props;
+    return (
+      <div>
+        Thank you for playing. The outcome of this game was:
+        <br />{outcome || 'Unknown'}
+      </div>
+    );
+  }
+}
+
+exports.Timeout = class extends React.Component {
+  render() {
+    return (
+      <div>
+        There's been a timeout. (Someone took too long.)
+      </div>
+    );
+  }
+}
+
+export default exports;
